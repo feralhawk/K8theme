b6ac779f1b98ca08a5dc8416583ef996ac16fc34
feralhawk <39734822+feralhawk@users.noreply.github.com>
2018-07-23 19:12:51 +0200

    k8theme

---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace k8theme\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+
+	}
+}
