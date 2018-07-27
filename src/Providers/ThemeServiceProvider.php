5cc38b89b1b5fc52e03cd46606de3e85b8dcde70
feralhawk <39734822+feralhawk@users.noreply.github.com>
2018-07-27 15:36:52 +0200

    edit ThemeServiceProvider

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace k8theme\Providers;
 
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class ThemeServiceProvider extends ServiceProvider
 {
