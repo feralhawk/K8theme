6549f9dde8dbc71af218c950361ec4413afd6e6d
feralhawk <39734822+feralhawk@users.noreply.github.com>
2018-08-07 17:32:44 +0200

    oarsch

--- src/Providers/K8themeServiceProvider.php
+++ src/Providers/K8themeServiceProvider.php
@@ -1,1 +1,20 @@
+<?php
+namespace K8theme\Providers;
 
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class HelloWorldServiceProvider
+ * @package HelloWorld\Providers
+ */
+class K8themeServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+		$this->getApplication()->register(K8themeRouteServiceProvider::class);
+	}
+}
