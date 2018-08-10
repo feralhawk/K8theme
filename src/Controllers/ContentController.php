0f496c9cfaf13c31ae1a8b95cd41992d0cb2d21b
feralhawk <39734822+feralhawk@users.noreply.github.com>
2018-08-10 17:01:02 +0200

    edit ContentController

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -12,9 +12,4 @@ class ContentController extends Controller
     {
         return $twig->render('K8theme::content.hello');
     }
-
-    public function showMyAccount(Twig $twig):string
-    {
-        return $twig->render('K8theme::MyAccount.MyAccount')
-    }
 }
