1b8e0f2246cbc4dc0eeaa5132bdf8e1203317e66
feralhawk <39734822+feralhawk@users.noreply.github.com>
2018-08-21 17:19:07 +0200

    + documents ,fonts, images, js, lang

---
+++ resources/js/src/app/exceptions/ExceptionMap.js
@@ -0,0 +1,13 @@
+export const exceptionMap = new Map(
+    [
+        ["1", "notificationsItemNotAdded"],
+        ["2", "notificationsNotEnoughStockItem"],
+        ["3", "notificationsInvalidResetPasswordUrl"],
+        ["4", "notificationsCheckPassword"],
+        ["301", "notificationRemoveCouponMinimumOrderValueIsNotReached"],
+        ["401", "notificationsCalculateShippingFailed"]
+    ]
+);
+
+export default exceptionMap;
+
