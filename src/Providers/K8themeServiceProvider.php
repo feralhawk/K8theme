d293db938ee2d731b8936ee257417d8a96d2794b
feralhawk <39734822+feralhawk@users.noreply.github.com>
2018-12-13 16:19:21 +0100

    change $eventDispatcher to $Dispatcher in K8themeServiceProvider.php

--- src/Providers/K8themeServiceProvider.php
+++ src/Providers/K8themeServiceProvider.php
@@ -247,7 +247,7 @@ class K8themeServiceProvider extends ServiceProvider
         }
 
         // K8theme Contexts
-        $eventDispatcher->listen('IO.ctx.item', function (TemplateContainer $templateContainer, $templateData = [])
+        $Dispatcher->listen('IO.ctx.item', function (TemplateContainer $templateContainer, $templateData = [])
          {
              $templateContainer->setContext( K8themeSingleItemContext::class);
              return false;
